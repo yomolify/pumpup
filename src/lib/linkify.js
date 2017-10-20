106733012451848540ba151dbdd92f1a73b23ec8
Amol Sidhu <sidhu.amol@gmail.com>
2017-10-20 10:30:37 -0700

    Implemented parsing logic for hashtags and mentions

---
+++ src/lib/linkify.js
@@ -0,0 +1,49 @@
+const linkify = require('linkify-it')()
+
+linkify.add('@', {
+  validate: function (text, pos, self) {
+    let tail = text.slice(pos)
+    if (!self.re.pumpup) {
+      self.re.pumpup =  new RegExp(
+        '^([a-zA-Z0-9_]){1,15}(?!_)(?=$|' + self.re.src_ZPCc + ')'
+      );
+    }
+    if (self.re.pumpup.test(tail)) {
+      // Linkifier allows punctuation chars before prefix,
+      // but we additionally disable `@` ("@@mention" is invalid)
+      if (pos >= 2 && tail[pos - 2] === '@') {
+        return false
+      }
+      return tail.match(self.re.pumpup)[0].length
+    }
+    return 0
+  },
+  normalize: function (match) {
+    match.url = 'https://pumpup.com/' + match.url.replace(/^@/, '')
+  }
+})
+
+linkify.add('#', {
+  validate: function (text, pos, self) {
+    let tail = text.slice(pos)
+    if (!self.re.pumpup) {
+      self.re.pumpup =  new RegExp(
+        '^([a-zA-Z0-9_]){1,15}(?!_)(?=$|' + self.re.src_ZPCc + ')'
+      );
+    }
+    if (self.re.pumpup.test(tail)) {
+      // Linkifier allows punctuation chars before prefix,
+      // but we additionally disable `@` ("@@mention" is invalid)
+      if (pos >= 2 && tail[pos - 2] === '#') {
+        return false
+      }
+      return tail.match(self.re.pumpup)[0].length
+    }
+    return 0
+  },
+  normalize: function (match) {
+    match.url = 'https://pumpup.com/' + match.url.replace(/^#/, '')
+  }
+})
+
+export default { linkify }
